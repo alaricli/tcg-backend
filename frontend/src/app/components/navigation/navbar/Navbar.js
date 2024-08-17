e24fc4b4face74b71d36e31c051d99ac1e0869ec
alaricli <alaricli@outlook.com>
2024-08-16 17:39:01 -0700

    Chore: update ESLint/Prettier/Husky configurations

--- frontend/src/app/components/navigation/navbar/Navbar.js
+++ frontend/src/app/components/navigation/navbar/Navbar.js
@@ -8,7 +8,16 @@ const Navbar = () => {
           <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/pages/cards">Home</Link>
+          <Link href="/pages/cards">Decks</Link>
+        </li>
+        <li>
+          <Link href="/pages/cards">Cards</Link>
+        </li>
+        <li>
+          <Link href="/pages/cards">Tournaments</Link>
+        </li>
+        <li>
+          <Link href="/pages/cards">Blog</Link>
         </li>
       </ul>
     </nav>
--- frontend/src/app/pages/Cards.js
+++ frontend/src/app/pages/Cards.js
@@ -1,7 +1,6 @@
-const { default: Head } = require('next/head');
-const { default: Link } = require('next/link');
+import Head from 'next/head';
+import Link from 'next/link';
 
-// pages/newpage.jsimportHeadfrom"next/head";
 const NewPage = () => {
   return (
     <>
@@ -10,7 +9,7 @@ const NewPage = () => {
         <meta name="description" content="Description of New Page" />
       </Head>
       <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-bold my-6">Welcome to New Page</h1>
+        <h1 className="my-6 text-2xl font-bold">Welcome to New Page</h1>
         <p>This is a new page in your Next.js application.</p>
         <Link href="/">
           <a className="text-blue-600 hover:underline">Go back home</a>
