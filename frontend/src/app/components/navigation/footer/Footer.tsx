c5112587224c6d49effb73dfdeaedb17dabf45fb
alaricli <alaricli@outlook.com>
2024-12-01 12:26:06 -0800

    initialize frontend

---
+++ frontend/src/app/components/navigation/footer/Footer.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white">
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-items-center">
+          {/* Column 1: Logo */}
+          <div>
+            <h3>Logo Here</h3>
+          </div>
+          {/* Column 2: Links */}
+          <div>
+            <ul className="">
+              <li>
+                <Link href="/">
+                  <p className="hover:text-orange-400">Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/articles">
+                  <p className="hover:text-orange-400">Articles</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/cards">
+                  <p className="hover:text-orange-400">Cards</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/decks">
+                  <p className="hover:text-orange-400">Decks</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/meta">
+                  <p className="hover:text-orange-400">Meta</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* Column 3: About Data */}
+          <div>
+            <ul>
+              <li>
+                <Link href="/">
+                  <p className="hover:text-orange-400">About</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p className="hover:text-orange-400">Contact</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p className="hover:text-orange-400">Privacy</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p className="hover:text-orange-400">Terms</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="mt-8 border-t border-gray-700 pt-4 text-center">
+          <p>&copy; 2024 Professor's Research TCG. All rights reserved.</p>
+          <p className="text-xs mt-1">
+            Pokémon and All Respective Names are Trademark & © of Nintendo.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/app/components/navigation/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-900 p-4">
+      <div className="flex justify-center">
+        <ul className="flex space-x-6 text-white font-medium text-xl">
+          <li>
+            <Link href="/">
+              <p className="hover:text-orange-400">Home</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/articles">
+              <p className="hover:text-orange-400">Articles</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/cards">
+              <p className="hover:text-orange-400">Cards</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/decks">
+              <p className="hover:text-orange-400">Decks</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/meta">
+              <p className="hover:text-orange-400">Meta</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
