0f7ad38faa5679bb152c260d5a7daf0d62e341cd
alaricli <alaricli@outlook.com>
2024-12-13 22:21:49 -0800

    moving frontend files to seperate repo

--- frontend-admin/app/routes/decks.tsx
+++
@@ -1,9 +0,0 @@
-const DecksManagementPage = () => {
-  return (
-    <div>
-      <h1>Decks Management</h1>
-    </div>
-  );
-};
-
-export default DecksManagementPage;
--- frontend-admin/app/routes/tournaments.tsx
+++
@@ -1,9 +0,0 @@
-const TournamentsManagementPage = () => {
-  return (
-    <div>
-      <h1>TournamentsManagementPage</h1>
-    </div>
-  );
-};
-
-export default TournamentsManagementPage;
